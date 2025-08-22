9d7842a0732ef1b08485c694f702a32b1212f735
Alex Jenter <AlexJenter@users.noreply.github.com>
2025-08-22 18:18:30 +0200

    Initial commit

---
+++ app/app.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const { status, data } = await useFetch("/api/usage", { lazy: true });
+</script>
+
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Date</th>
+        <th>Name</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="{ date, name } in data">
+        <td>{{ date }}</td>
+        <td>{{ name }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
---
+++ server/api/usage.ts
@@ -0,0 +1,29 @@
+import fs from "fs";
+
+
+interface RowItem {
+  date: string
+  name: string
+}
+
+export default defineEventHandler((event) => {
+  try {
+    const directory =
+      "/Users/alex/Library/Mobile Documents/iCloud~is~workflow~my~workflows/Documents/Public";
+    const usageFilename = `${directory}/usage.txt`;
+    const productsFilename = `${directory}/products.json`;
+
+    const usageString = fs.readFileSync(usageFilename, "utf8");
+    const productsString = fs.readFileSync(productsFilename, "utf8");
+    const productsData = JSON.parse(productsString);
+
+    const data = usageString
+      .split("\n")
+      .map((str) => str.split(" "))
+      .map(([date, ean]) => (<RowItem>{ date, name: productsData[ean] || "" }));
+
+    return data
+  } catch (err) {
+    throw Error(err + "error fetching product names");
+  }
+});
