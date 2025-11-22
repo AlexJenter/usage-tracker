f90004f4ad7ef226c199df24f80e5abdcdbdb93b
Alex Jenter <AlexJenter@users.noreply.github.com>
2025-11-22 14:26:02 +0100

    Trigger webhook / deploy

---
+++ server/api/private/track-usage/[ean].ts
@@ -0,0 +1,22 @@
+import fs from "fs";
+
+import {
+  defineEventHandler,
+  readBody,
+  getHeader,
+  sendError,
+  createError,
+} from "h3";
+
+export default defineEventHandler(async (event) => {
+  const ean = getRouterParam(event, "ean");
+  const directory = process.env.PERSISTENT_STORAGE_DIRECTORY || "./storage";
+
+  const [date] = new Date().toISOString().split("T");
+
+  return {
+    ok: true,
+    msg: "You passed the secret check 🎉 privat area",
+    value: `${date} ${ean}`,
+  };
+});
