7824f2d96b9232d8776e710501e1a1cf2df40f8b
Alex Jenter <AlexJenter@users.noreply.github.com>
2025-11-22 14:50:28 +0100

    Unify storage across environments

--- server/api/usage.ts
+++ server/api/usage.ts
@@ -1,18 +1,16 @@
 import fs from "fs";
 
-
 interface RowItem {
-  date: string
-  name: string
-  month: number
-  monthName: string
-  ean: string
+  date: string;
+  name: string;
+  month: number;
+  monthName: string;
+  ean: string;
 }
 
 export default defineEventHandler((event) => {
   try {
-    const directory =
-      "/Users/alex/Library/Mobile Documents/iCloud~is~workflow~my~workflows/Documents/Public";
+    const directory = process.env.PERSISTENT_STORAGE_DIRECTORY || "./storage";
     const usageFilename = `${directory}/usage.txt`;
     const productsFilename = `${directory}/products.json`;
 
@@ -23,16 +21,21 @@ export default defineEventHandler((event) => {
     const data = usageString
       .split("\n")
       .map((str) => str.split(" "))
-      .map(([date, ean]) => (<RowItem>{
-        date,
-        month: new Date(date).getMonth(),
-        monthName: new Date(date).toLocaleString("default", { month: "long" }),
-        name: productsData[ean] || "",
-        ean
-      }))
+      .map(
+        ([date, ean]) =>
+          <RowItem>{
+            date,
+            month: new Date(date).getMonth(),
+            monthName: new Date(date).toLocaleString("default", {
+              month: "long",
+            }),
+            name: productsData[ean] || "",
+            ean,
+          },
+      )
       .reverse();
 
-    return data
+    return data;
   } catch (err) {
     throw Error(err + "error fetching product names");
   }
